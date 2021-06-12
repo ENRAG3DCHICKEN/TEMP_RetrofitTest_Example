2e78258ea69499f298893071789ebff991a302b7
ENRAG3DCHICKEN <yiu.jackie@gmail.com>
2021-06-12 15:40:53 -0400

    Retrofit Example (Kotlin) - Weather

---
+++ app/src/main/java/com/example/retrofittest/WeatherService.kt
@@ -0,0 +1,18 @@
+package com.example.retrofittest
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.retrofittest.databinding.ActivityWeatherServiceBinding
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherService {
+    @GET("data/2.5/weather?")
+    fun getCurrentWeatherData(@Query("lat") lat: String, @Query("lon") lon: String, @Query("APPID") app_id: String): Call<WeatherResponse>
+}
